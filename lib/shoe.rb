9c0313d1b6534bcc48d71e0a172d8969e78362bb
Matthew Todd <matthew.todd@gmail.com>
2010-02-19 17:17:41 +0300

    Tidy up some rdoc.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,22 +1,7 @@
-# Shoe defines some handy Rake tasks for your project, all built around your
-# Gem::Specification.
-#
-# Here's how you use it in your Rakefile:
-#
-#  require 'shoe'
-#  Shoe.tie('myproject', '0.1.0', "This is my project, and it's awesome!") do |spec|
-#    # do whatever you want with the Gem::Specification here, for example:
-#    # spec.add_runtime_dependency 'dnssd'
-#  end
-#
-# Shoe comes with an executable named "shoe" that will generate a Rakefile like
-# this (but slightly fancier) for you.
 module Shoe
   autoload :Project, 'shoe/project'
   autoload :Tasks,   'shoe/tasks'
 
-  # Here's where you start. In your Rakefile, you'll probably just call
-  # Shoe.tie, then add any dependencies in the block.
   def self.tie(name, version, summary)
     project = Project.new(name, version, summary)
     yield project.spec if block_given?
