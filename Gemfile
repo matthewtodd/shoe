c77f0a28d0ee68f21cdb23f1120c86874f544c74
Matthew Todd <matthew.todd@gmail.com>
2010-03-30 15:52:05 +0300

    Fix the features when running with latest bundler.
    commit c38cd945989cf3cc8a73da2f6883f8a42227b140
    Author: Matthew Todd <matthew.todd@gmail.com>
    Date:   Tue Mar 30 15:50:54 2010 +0300
    
        Cleanup.
    
    commit f60d64cf00389aa192c688070c68433064f5790a
    Author: Matthew Todd <matthew.todd@gmail.com>
    Date:   Tue Mar 30 15:50:27 2010 +0300
    
        Get the features working again!
    
    commit fded4170f3b3310d9472312c800acaf59df06217
    Author: Matthew Todd <matthew.todd@gmail.com>
    Date:   Tue Mar 30 15:31:33 2010 +0300
    
        Include bundler in the Gemfile.
    
    commit 5b84173cc95781fb17aa07e011c4f6ff305bac8e
    Author: Matthew Todd <matthew.todd@gmail.com>
    Date:   Tue Mar 30 14:30:35 2010 +0300
    
        Preserve GEM_HOME and GEM_PATH so features work.
    
    commit 0fe330e13587d47e1eb84caa32cee4f0f3722e99
    Author: Matthew Todd <matthew.todd@gmail.com>
    Date:   Tue Mar 30 14:23:39 2010 +0300
    
        Guard against spaces in paths.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source :rubygems
 
 gem 'shoe', :path => File.expand_path('..', __FILE__)
 
+gem 'bundler'
 gem 'cucumber'
 gem 'rake'
--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,7 @@ require 'test/unit/assertions'
 require 'tmpdir'
 
 class WorkingDirectory
-  PROJECT_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..', '..')))
+  PROJECT_ROOT = Pathname.new(File.expand_path('../../..', __FILE__))
 
   attr_reader :working_directory
   attr_reader :standard_out
@@ -48,13 +48,22 @@ class WorkingDirectory
   private
 
   def be_sneaky_with_the_gemfile(contents)
-    contents.sub("gem 'shoe'", "gem 'shoe', :path => '#{Pathname.pwd.expand_path}'")
+    contents.sub("gem 'shoe'", "gem 'shoe', :path => '#{PROJECT_ROOT.expand_path}'")
   end
 
-  # bundle exec rake running from bundle exec cucumber otherwise gets confused
-  # about where the real Gemfile is.
   def isolate_environment(command)
-    "/usr/bin/env -i HOME=#{ENV['HOME']} PATH=#{ENV['PATH']} #{command}"
+    # set the PATH so bundle exec can find shoe
+    ENV['PATH'] = ENV['PATH'].split(File::PATH_SEPARATOR).
+                            unshift(PROJECT_ROOT.join('bin')).uniq.
+                               join(File::PATH_SEPARATOR)
+
+    # whack most environment variables so nested bundle exec won't get confused
+    # about where the Gemfile is
+    "/usr/bin/env -i #{preserve_environment 'HOME', 'PATH', 'GEM_HOME', 'GEM_PATH'} #{command}"
+  end
+
+  def preserve_environment(*variables)
+    variables.map { |name| "#{name}='#{ENV[name]}'" }.join(' ')
   end
 end
 
