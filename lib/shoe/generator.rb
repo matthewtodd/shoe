ffd6d0993789484b4d4b299d119c3bbb1d87c71f
Matthew Todd <matthew.todd@gmail.com>
2010-04-07 14:34:56 +0300

    No need to put self in the Gemfile.
    Since the generated gemspec tweaks the LOAD_PATH by itself now.

--- lib/shoe/generator.rb
+++ lib/shoe/generator.rb
@@ -10,7 +10,6 @@ module Shoe
 
     def run
       path('README.rdoc').install template('readme.erb')
-      path('Gemfile').update      template('gemfile.erb')
       path('Rakefile').install    template('rakefile.erb')
       path(version_path).install  template('version.erb')
       path(gemspec_path).install  template('gemspec.erb')
@@ -57,22 +56,8 @@ module Shoe
         if exist?
           $stderr.puts "#{to_s} exists. Not clobbering."
         else
-          write(contents, 'w')
+          open('w') { |file| file.write(contents) }
         end
-      end
-
-      def update(contents)
-        if exist?
-          write(contents, 'a')
-        else
-          $stderr.puts "#{to_s} does not exist. Not updating."
-        end
-      end
-
-      private
-
-      def write(contents, mode)
-        open(mode) { |file| file.write(contents) }
       end
     end
   end
