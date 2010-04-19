4a99ec435fef45365181b8d9f360d2baa8906391
Matthew Todd <matthew.todd@gmail.com>
2010-04-19 07:52:59 +0300

    shoe --application makes an executable stub.

--- features/getting_started.feature
+++ features/getting_started.feature
@@ -16,6 +16,21 @@ Feature: Getting started
     And I should see a file "my_project/lib/my_project.rb"
     And I should see a file "my_project/my_project.gemspec"
 
+  Scenario: Running shoe --application in an empty directory
+    Given I have created a directory called "my_project"
+    And I have created a file called "my_project/Gemfile" containing:
+      """
+      source :rubygems
+      gem 'shoe', :group => :development
+      """
+    When I run bundle exec shoe --application . inside "my_project"
+    Then I should see a file "my_project/Rakefile"
+    And I should see a file "my_project/README.rdoc"
+    And I should see a file "my_project/bin/my_project"
+    And I should see a file "my_project/lib/my_project.rb"
+    And I should see a file "my_project/lib/my_project/application.rb"
+    And I should see a file "my_project/my_project.gemspec"
+
   Scenario: Running shoe with no arguments in a directory that already has a Rakefile
     Given I have created a directory called "my_project"
     And I have created a file called "my_project/Gemfile" containing:
--- lib/shoe/generator.rb
+++ lib/shoe/generator.rb
@@ -5,25 +5,39 @@ require 'pathname'
 module Shoe
   class Generator
     def initialize
-      @options = OptionParser.new do |opts|
-        opts.banner  = "Usage: #{File.basename($0)} [path]"
-        opts.version = Shoe::VERSION
-      end
+      @defaults = %w(--no-application .)
 
-      @path = Pathname.pwd
+      @options  = OptionParser.new do |opts|
+        opts.banner  = "Usage: #{File.basename($0)} [options] [path]"
+        opts.version = Shoe::VERSION
+
+        opts.separator ''
+        opts.on('-a', '--[no-]application', 'Generate a command-line application.') do |application|
+          @application = application
+        end
+
+        opts.separator ''
+        opts.separator 'Defaults:'
+        opts.separator opts.summary_indent + @defaults.join(' ')
+      end
     end
 
     def run(argv)
       begin
-        @options.order(argv) { |path| @path = Pathname.new(path) }
+        @options.order(@defaults.concat(argv)) { |path| @path = Pathname.new(path) }
       rescue OptionParser::ParseError
         @options.abort($!)
       end
 
       path('README.rdoc').install template('readme.erb')
       path('Rakefile').install    template('rakefile.erb')
-      path(module_path).install   template('module.erb')
       path(gemspec_path).install  template('gemspec.erb')
+      path(module_path).install   template('module.erb')
+
+      if application?
+        path(executable_path).install  template('executable.erb'), 0755
+        path(application_path).install template('application.erb')
+      end
     end
 
     private
@@ -40,16 +54,28 @@ module Shoe
       '0.0.0'
     end
 
-    def module_path
-      "lib/#{project_name}.rb"
+    def application?
+      @application
+    end
+
+    def application_path
+      "lib/#{project_name}/application.rb"
+    end
+
+    def executable_path
+      "bin/#{project_name}"
     end
 
     def gemspec_path
       "#{project_name}.gemspec"
     end
 
+    def module_path
+      "lib/#{project_name}.rb"
+    end
+
     def template(name)
-      ERB.new(template_contents(name)).result(binding)
+      ERB.new(template_contents(name), nil, '<>').result(binding)
     end
 
     def template_contents(name)
@@ -63,11 +89,12 @@ module Shoe
     end
 
     module PathExtensions #:nodoc:
-      def install(contents)
+      def install(contents, mode=0644)
         if exist?
           $stderr.puts "#{to_s} exists. Not clobbering."
         else
           open('w') { |file| file.write(contents) }
+          chmod(mode)
         end
       end
     end
