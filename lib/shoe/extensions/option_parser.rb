480b70ee73fe28d6c0af9e315101aeb1605a0dff
Matthew Todd <matthew.todd@gmail.com>
2010-04-22 14:04:51 +0300

    Clean up and document OptionParser.

--- lib/shoe/extensions/option_parser.rb
+++ lib/shoe/extensions/option_parser.rb
@@ -1,23 +1,52 @@
 module Shoe
   module Extensions
 
+    # This extension allows for default options in OptionParser, processing
+    # them before any given options and appending them nicely to the help
+    # message. (It also does the conventional thing and errors with usage when
+    # there's trouble parsing options.)
+    #
+    # Note that it's important to use #order rather than #permute, to ensure
+    # that the given options have a chance to override the defaults. (These are
+    # <tt>POSIX</tt>-ly correct semantics.)
+    #
+    # To use:
+    #   parser = OptionsParser.new do |opts|
+    #              opts.extend(Shoe::Extensions::OptionParser)
+    #              opts.defaults = %w(--foo --no-bar --baz=5)
+    #              # ... and so on ...
+    #            end
+    #
+    #   parser.order(ARGV)
     module OptionParser
-      attr_accessor :defaults
+      attr_reader :defaults
+
+      def defaults=(defaults)
+        @defaults = defaults.extend(Defaults)
+      end
 
       def order(*args, &block)
         begin
-          super(defaults.dup.concat(*args), &block)
+          super(*defaults.followed_by(*args), &block)
         rescue ::OptionParser::ParseError
           abort($!)
         end
       end
 
-      def summarize(*args, &block)
-        return <<-END.gsub(/^ {10}/, '')
-          #{super}
-          Defaults:
-          #{summary_indent}#{defaults.join(' ')}
-        END
+      def help
+        defaults.help(super, summary_indent)
+      end
+
+      private
+
+      module Defaults #:nodoc:
+        def followed_by(*args)
+          dup.concat(*args)
+        end
+
+        def help(before, indent)
+          "#{before}\nDefaults:\n#{indent}#{join ' '}"
+        end
       end
     end
 
