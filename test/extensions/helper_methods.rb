1ac47686149ea9ec22c43b1d46ce0b8328d9719a
Matthew Todd <matthew.todd@gmail.com>
2010-10-10 16:33:31 +0300

    Remove more dead code.

--- test/extensions/helper_methods.rb
+++ test/extensions/helper_methods.rb
@@ -1,4 +1,3 @@
-require 'open3'
 require 'pathname'
 
 module Shoe
--- test/extensions/isolated_environment.rb
+++ test/extensions/isolated_environment.rb
@@ -5,29 +5,16 @@ module Shoe
 
     module IsolatedEnvironment
       def setup
-        @environment = Environment.new
-        @environment.setup
+        @initial_directory = Dir.pwd
+        @working_directory = Dir.mktmpdir
+        Dir.chdir(@working_directory)
         super
       end
 
       def teardown
         super
-        @environment.teardown
-      end
-
-      private
-
-      class Environment
-        def setup
-          @initial_directory   = Dir.pwd
-          @working_directory   = Dir.mktmpdir
-          Dir.chdir(@working_directory)
-        end
-
-        def teardown
-          Dir.chdir(@initial_directory)
-          FileUtils.remove_entry_secure(@working_directory)
-        end
+        Dir.chdir(@initial_directory)
+        FileUtils.remove_entry_secure(@working_directory)
       end
     end
 
