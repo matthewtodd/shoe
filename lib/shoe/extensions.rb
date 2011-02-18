e36c22545c95ae59cdf6c2169027e042b026239c
Matthew Todd <matthew@matthewtodd.org>
2011-02-18 10:13:18 +0300

    Switch from Gem::Validator to testrb.

--- lib/shoe/extensions.rb
+++ lib/shoe/extensions.rb
@@ -2,6 +2,5 @@ module Shoe
   module Extensions
     autoload :DocManager,    'shoe/extensions/doc_manager'
     autoload :Specification, 'shoe/extensions/specification'
-    autoload :Validator,     'shoe/extensions/validator'
   end
 end
--- lib/shoe/tasks/test.rb
+++ lib/shoe/tasks/test.rb
@@ -1,15 +1,9 @@
-require 'rubygems/validator'
-
 module Shoe
   module Tasks
 
     # Defines <tt>`rake test`</tt> to run your tests.
     #
-    # Uses
-    # <tt>{Gem::Validator}[http://rubygems.rubyforge.org/rubygems-update/Gem/Validator.html]</tt>,
-    # so tests are run locally just as they will be with <tt>`{gem
-    # check}[http://docs.rubygems.org/read/chapter/10#page30] --test
-    # your_project`</tt>.
+    # Uses <tt>testrb</tt> to run your <tt>test_files</tt>.
     #
     # To enable and configure, edit the
     # <tt>test_files[http://docs.rubygems.org/read/chapter/20#test_files]</tt>
@@ -17,25 +11,14 @@ module Shoe
     #
     # = A Few Precautions
     #
-    # * Using <tt>Gem::Validator</tt> means that only <tt>Test::Unit</tt> (all
+    # * Using <tt>testrb</tt> means that only <tt>Test::Unit</tt> (all
     #   Rubies) and <tt>MiniTest::Unit</tt> (Ruby 1.9) tests will be run.
     #
     # * You'll need to <tt>Bundler.setup(:default, :development)</tt> in your
-    #   <tt>Rakefile</tt> so that the <tt>Gem::Validator</tt> can find your
-    #   code. (If you put this line in your <tt>test_helper</tt> instead,
-    #   you'll break <tt>`gem check --test`</tt> for people who don't have
-    #   Bundler installed.)
+    #   <tt>Rakefile</tt> so that <tt>testrb</tt> can find your code.
     #
     # * The <tt>test</tt> directory will not be in the <tt>$LOAD_PATH</tt>, so
     #   you'll have to <tt>require 'test/test_helper'</tt>.
-    #
-    # = Pretty Colors
-    #
-    # For Ruby 1.8, Shoe patches the <tt>Gem::Validator</tt> to play nicely with
-    # {redgreen}[http://rubygems.org/gems/redgreen]</tt>.
-    #
-    # For Ruby 1.9, <tt>require 'shoe/util/minitest_colors'</tt> in your
-    # <tt>test_helper</tt>.
     #
     class Test < Task
       def active?
@@ -49,8 +32,7 @@ module Shoe
         END
 
         task :test do
-          Gem::Validator.extend(Extensions::Validator)
-          Gem::Validator.new.unit_test(spec)
+          system 'testrb', *spec.test_files
         end
 
         task :prepare
