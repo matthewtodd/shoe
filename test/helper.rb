079b51f56cabd819c9a231d30c2c37d4e3c5583e
Matthew Todd <matthew@matthewtodd.org>
2011-02-21 16:31:26 +0300

    Unroll clever loop.

--- test/helper.rb
+++ test/helper.rb
@@ -11,6 +11,11 @@ class Shoe::TestCase < Test::Unit::TestCase
   end
 end
 
-Dir.chdir('test') do
-  Dir['support/*.rb'].each { |path| require path }
-end
+require 'support/assertions'
+require 'support/command_runner'
+require 'support/declarative_tests'
+require 'support/example_project'
+require 'support/file_manipulation'
+require 'support/gemspec_manipulation'
+require 'support/project_files'
+require 'support/redgreen'
