10ef30a422d51f38d9942152dbfd10e43b5d05df
Matthew Todd <matthew@matthewtodd.org>
2011-02-21 16:36:56 +0300

    Remove redgreen load attempt.

--- test/support/redgreen.rb
+++
@@ -1,6 +0,0 @@
-begin
-  require 'redgreen' if $stdout.tty?
-rescue LoadError
-  # Since we don't have hard gem dependencies for testing, folks can run `gem
-  # check --test shoe` without installing anything else.
-end
